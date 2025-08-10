f43aa87220b71bbb88535da06f12afd45484c684
Austin Riba <austin@m51.io>
2025-08-09 21:06:15 -0700

    Use static gamma lookup table

--- src/scaling.rs
+++ src/scaling.rs
@@ -1,6 +1,25 @@
 use ndarray::{Array, Array2, Axis, stack};
 use ndarray_linalg::LeastSquaresSvd;
 
+/// Generate this table using the gamma_lookup_table() function
+/// This saves 20-30ms as opposed to calculating it at runtime
+static GAMMA_LOOKUP: [u8; 256] = [
+    0, 27, 36, 43, 48, 53, 57, 60, 64, 67, 70, 72, 75, 77, 80, 82, 84, 86, 88, 90, 92, 94, 96, 97,
+    99, 101, 102, 104, 105, 107, 108, 110, 111, 112, 114, 115, 116, 118, 119, 120, 122, 123, 124,
+    125, 126, 127, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144,
+    145, 146, 147, 148, 149, 149, 150, 151, 152, 153, 154, 155, 156, 156, 157, 158, 159, 160, 161,
+    161, 162, 163, 164, 164, 165, 166, 167, 168, 168, 169, 170, 171, 171, 172, 173, 173, 174, 175,
+    176, 176, 177, 178, 178, 179, 180, 180, 181, 182, 182, 183, 184, 184, 185, 186, 186, 187, 188,
+    188, 189, 189, 190, 191, 191, 192, 193, 193, 194, 194, 195, 196, 196, 197, 197, 198, 199, 199,
+    200, 200, 201, 201, 202, 203, 203, 204, 204, 205, 205, 206, 207, 207, 208, 208, 209, 209, 210,
+    210, 211, 211, 212, 212, 213, 214, 214, 215, 215, 216, 216, 217, 217, 218, 218, 219, 219, 220,
+    220, 221, 221, 222, 222, 223, 223, 224, 224, 225, 225, 226, 226, 227, 227, 228, 228, 229, 229,
+    229, 230, 230, 231, 231, 232, 232, 233, 233, 234, 234, 235, 235, 235, 236, 236, 237, 237, 238,
+    238, 239, 239, 239, 240, 240, 241, 241, 242, 242, 243, 243, 243, 244, 244, 245, 245, 246, 246,
+    246, 247, 247, 248, 248, 249, 249, 249, 250, 250, 251, 251, 251, 252, 252, 253, 253, 253, 254,
+    254, 255, 255, 255,
+];
+
 #[derive(Debug)]
 #[allow(dead_code)]
 struct ZscaleBounds {
@@ -63,12 +82,15 @@ fn least_squares_line_fit(sample_data: &[f32]) -> LeastSquareResult {
     }
 }
 
-fn gamma_adjust_table() -> Vec<u8> {
-    let size = 255 + 1; // Max size minus min size plus 1
-    let mut table = vec![0; size];
-    (0..size).for_each(|i| {
-        table[i] = (size as f32 * (i as f32 / 255.0).powf(1.0 / 2.5)) as u8;
-    });
+fn gamma_adjust_table() -> [u8; 256] {
+    // If powf ever becomes constified, we can use this as a const function
+    // instead of hardcoding the GAMMA_LOOKUP table like we do now.
+    let mut table = [0; 256];
+    let mut i = 0;
+    while i < 256 {
+        table[i] = (256.0 * (i as f32 / 255.0).powf(1.0 / 2.5)) as u8;
+        i += 1;
+    }
     table
 }
 
@@ -87,10 +109,9 @@ fn linear_scale(mut image_data: Vec<f32>, zmin: f32, zmax: f32) -> Vec<u8> {
         *pixel -= adjust;
         *pixel = pixel.round();
     }
-    let gamma_lookup = gamma_adjust_table();
     image_data
         .into_iter()
-        .map(|e| gamma_lookup[e as usize])
+        .map(|e| GAMMA_LOOKUP[e as usize])
         .collect()
 }
 
@@ -132,5 +153,11 @@ mod tests {
             DynamicImage::ImageLuma8(ImageBuffer::from_vec(2400, 2400, scaled).unwrap());
         image = image.resize(300, 300, image::imageops::FilterType::Triangle);
         image.save("tests/output/random.jpeg").unwrap();
+    }
+
+    #[test]
+    fn test_gamma_adjust() {
+        let table = gamma_adjust_table();
+        assert_eq!(table, GAMMA_LOOKUP);
     }
 }
